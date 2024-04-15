f97ab7c56da88de3fc27f5567c69790b3efe439a
Jameel Ahmed <jameel.webdev@gmail.com>
2024-04-15 14:59:31 +0000

    ip

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get("/api/whoami", (req, res) => {
   const ipaddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   const language = req.headers["accept-language"];
   const software = req.headers["user-agent"];
-
   res.json({
     ipaddress,
     language,
